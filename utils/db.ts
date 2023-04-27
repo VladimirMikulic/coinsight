4ce30817d8be4f0086eaa6f587dfabcc67d5e807
Vladimir <vladimir.mikulic2@gmail.com>
2023-04-27 11:43:43 +0200

    ADD: Retry mechanism to getCurrencyPrices

--- utils/db.ts
+++ utils/db.ts
@@ -27,10 +27,14 @@ const getCurrencyPrices = async (
             new Date(fromDate).getTime() + index * 86400 * 1000
           );
 
-          return MindsDB.SQL.runQuery(
-            `SELECT * FROM mindsdb.${currency}_predictor WHERE date="${formatDate(
-              date
-            )}"`
+          return retry(
+            MindsDB.SQL.runQuery.bind(MindsDB.SQL),
+            [
+              `SELECT * FROM mindsdb.${currency}_predictor WHERE date="${formatDate(
+                date
+              )}"`,
+            ],
+            10
           );
         })
     );
@@ -44,5 +48,31 @@ const getCurrencyPrices = async (
     throw error;
   }
 };
+
+// Requests to MindsDB may occasionally or return result with an error message
+// In that case, simply retrying will resolve the issue.
+async function retry<T extends (...arg0: any[]) => any>(
+  fn: T,
+  args: Parameters<T>,
+  maxTry: number,
+  retryCount = 1
+): Promise<Awaited<ReturnType<T>>> {
+  const currRetry = typeof retryCount === 'number' ? retryCount : 1;
+  try {
+    const result = await fn(...args);
+    if (result.error_message) throw new Error(result.error_message);
+    return result;
+  } catch (e) {
+    console.log(`Retry ${currRetry} failed.`, e);
+    if (currRetry > maxTry) {
+      console.log(`All ${maxTry} retry attempts exhausted`);
+      throw e;
+    }
+    await new Promise(resolve => {
+      setTimeout(resolve, 5000);
+    });
+    return retry(fn, args, maxTry, currRetry + 1);
+  }
+}
 
 export default getCurrencyPrices;
